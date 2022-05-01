f6661ccd66fecb5655bdc97ad320da251468fecf
robertd23 <robertd23@unc.edu>
2022-04-30 21:30:31 -0400

    fixed minor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.static('./public'))
 app.use(express.json())
 const morgan = require('morgan')
 const fs = require('fs')
-const db = require('.src/services/database.js')
+const db = require('./src/services/database.js')
 
 const args = require("minimist")(process.argv.slice(2));
 args["port"];
